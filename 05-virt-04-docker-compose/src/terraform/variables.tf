ed7ad1dd613b38f6d8cd99712a9a435bf9416b66
korotkov-dmitry <fullautopilot@gmail.com>
2022-02-08 09:35:14 +0700

    src for VM

---
+++ 05-virt-04-docker-compose/src/terraform/variables.tf
@@ -0,0 +1,17 @@
+# Заменить на ID своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_cloud_id" {
+  default = "b1gtpsf9dfjk5055buns"
+}
+
+# Заменить на Folder своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_folder_id" {
+  default = "b1gc1tus5qie76988pha"
+}
+
+# Заменить на ID своего образа
+# ID можно узнать с помощью команды yc compute image list
+variable "centos-7-base" {
+  default = "fd8cu4vnuvbuimhsktc2"
+}
