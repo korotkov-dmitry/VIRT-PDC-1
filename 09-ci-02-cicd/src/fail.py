fa9b6baab8e4cecbbde7eacf11f8dbcc3f2d0f7c
korotkov-dmitry <fullautopilot@gmail.com>
2022-05-29 15:59:29 +0700

    HW

---
+++ 09-ci-02-cicd/src/fail.py
@@ -0,0 +1,12 @@
+def increment(index):
+    return index+1
+def get_square(numb):
+    return numb*numb
+def print_numb(numb):
+    print("Number is {}".format(numb))
+    pass
+
+index = 0
+while (index < 10):
+    index = increment(index)
+    print(get_square(index))
