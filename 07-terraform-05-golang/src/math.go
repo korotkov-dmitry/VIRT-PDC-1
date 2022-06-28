1463cc42a1026f641ffbd3e71a001010ca8e4f82
korotkov dmitry <92984527+korotkov-dmitry@users.noreply.github.com>
2022-06-28 17:05:47 +0700

    HW

---
+++ 07-terraform-05-golang/src/math.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Print("Enter a number: ")
+	var input float64
+
+	fmt.Scanf("%f", &input)
+
+	output := input * 0.3048
+
+	fmt.Println(output)
+}
